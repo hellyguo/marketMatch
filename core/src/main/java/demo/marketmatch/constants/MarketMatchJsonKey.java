1f1c32047d3411fea3fb1326bf78c52c86be5b65
Helly Guo <buffoonguo@gmail.com>
2016-10-07 21:58:19 +0800

    remove needless field: jsonValType

--- core/src/main/java/demo/marketmatch/constants/MarketMatchJsonKey.java
+++ core/src/main/java/demo/marketmatch/constants/MarketMatchJsonKey.java
@@ -4,27 +4,21 @@ package demo.marketmatch.constants;
  * Created by helly on 2016/9/30.
  */
 public enum MarketMatchJsonKey {
-    PID("pid", String.class),
-    CID("cid", String.class),
-    DIRECT("direct", MarketMatchDirect.class),
-    ORDER_TYPE("orderType", MarketMatchType.class),
-    LIMIT_PRICE("limitPrice", Integer.class),
-    VOLUME("volume", Integer.class),
-    TIMESTAMP("timestamp", Long.class);
+    PID("pid"),
+    CID("cid"),
+    DIRECT("direct"),
+    ORDER_TYPE("orderType"),
+    LIMIT_PRICE("limitPrice"),
+    VOLUME("volume"),
+    TIMESTAMP("timestamp");
 
     private String jsonKeyName;
-    private Class jsonValType;
 
-    MarketMatchJsonKey(String jsonKeyName, Class jsonValType) {
+    MarketMatchJsonKey(String jsonKeyName) {
         this.jsonKeyName = jsonKeyName;
-        this.jsonValType = jsonValType;
     }
 
-    public String jsonKeyName(){
+    public String jsonKeyName() {
         return jsonKeyName;
-    }
-
-    public <T> Class<T> jsonValType(){
-        return jsonValType;
     }
 }
